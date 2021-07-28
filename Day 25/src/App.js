8b2a9b672c39a75182a8a7a46854483984cae99c
Abhay_Singh_Bisht <singhbishtabhay02@gmail.com>
2021-07-28 19:56:45 +0530

    Tha

---
+++ Day 25/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav'
+import Card from './Card';
+function App() {
+  return (
+    <div>
+    
+    <Nav/>
+    <Card/>
+    
+   </div>
+  );
+}
+
+export default App;
